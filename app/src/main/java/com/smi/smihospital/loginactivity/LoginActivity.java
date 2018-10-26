67ccc9053e458467b19d5333e2d9cdd5145a0bf6
malavancs <mlvnhari@gmail.com>
2018-10-26 20:25:23 +0530

    Initial commit

---
+++ app/src/main/java/com/smi/smihospital/loginactivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.smi.smihospital.loginactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smi.smihospital.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
